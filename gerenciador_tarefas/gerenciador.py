42c0fd0bb30704ca92c4caf90b107979732b683c
kyabey <karolynebardemaker@gmail.com>
2022-05-18 19:47:08 -0300

    Adiciona recurso de adicionar tarefa

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
-TAREFAS = []
+TAREFAS = [{
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }]
 
 app = FastAPI()
 
