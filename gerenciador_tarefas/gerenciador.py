a5de287c330b0712b2b1ab6ced866f06fc21d748
kyabey <karolynebardemaker@gmail.com>
2022-05-18 19:50:15 -0300

    Adiciona recurso de listar tarefa

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
 
-TAREFAS = [{
-            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            "titulo": "titulo 1",
-            "descricao": "descricao 1",
-            "estado": "finalizado",
-        }]
+TAREFAS = []
 
 app = FastAPI()
 
